9caa027d107f002fcc2a0e0f12f6ca48ee5333dd
stack-guru <GoldenHand0302@gmail.com>
2025-05-12 16:48:09 -0500

    refactor: update some versions and settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,23 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200, // or try a lower value like 50 for slightly smaller size
+    },
+  },
+  outputSelection: {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "devdoc",
+        "userdoc",
+        "metadata",
+        "abi"
+      ]
+    }
+  },
+  remappings: []
 };
